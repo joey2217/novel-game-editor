5d5f2a8af1a9fdea823e55ff896b381b536a9bc5
Joey <786678257@qq.com>
2021-11-16 18:35:07 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {Container} from './style'
+
+function App() {
+  return (
+    <Container>
+       AAA
+    </Container>
+  );
+}
+
+export default App;
